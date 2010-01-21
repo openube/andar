ed97f44bdee898d235a93f679c2186887f788d06
tdomhan <tdomhan@258557a0-e19b-11de-9129-6395494df245>
2010-01-21 20:43:52 +0000

    native code clean up

--- trunk/AndOpenGLCam/jni/yuv420sp2rgb.h
+++ trunk/AndOpenGLCam/jni/yuv420sp2rgb.h
@@ -1,46 +1,10 @@
-typedef struct rgb_context {
-    unsigned char *buffer;
-    int width;
-    int height;
-    int rotate;
-    int i;
-    int j;
-    int size; /* for debugging */
-    int texture_size;
-} rgb_context;
+//yuv420sp2rgb.h
 
-typedef void (*rgb_cb)(
-    unsigned char r,
-    unsigned char g,
-    unsigned char b,
-    rgb_context *ctx);
-
-static void color_convert_common(
+static inline void color_convert_common(
     unsigned char *pY, unsigned char *pUV,
     int width, int height, int texture_size,
     unsigned char *buffer,
     int size, /* buffer size in bytes */
     int gray,
-    int rotate,
-    rgb_cb cb);
-
-
-static void rgb16_cb(
-    unsigned char r,
-    unsigned char g,
-    unsigned char b,
-    rgb_context *ctx);
-
-static void common_rgb_cb(
-    unsigned char r,
-    unsigned char g,
-    unsigned char b,
-    rgb_context *ctx,
-    int alpha);
-
-static void rgb24_cb(
-    unsigned char r,
-    unsigned char g,
-    unsigned char b,
-    rgb_context *ctx);
+    int rotate);
 
