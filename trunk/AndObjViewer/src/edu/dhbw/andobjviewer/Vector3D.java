f20794315fa590503761b07287a71e5aaaa605e2
tdomhan <tdomhan@258557a0-e19b-11de-9129-6395494df245>
2010-01-28 18:11:59 +0000

    forget some files

---
+++ trunk/AndObjViewer/src/edu/dhbw/andobjviewer/Vector3D.java
@@ -0,0 +1,78 @@
+/**
+	Copyright (C) 2009  Tobias Domhan
+
+    This file is part of AndObjViewer.
+
+    AndObjViewer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AndObjViewer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with AndObjViewer.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
+package edu.dhbw.andobjviewer;
+
+/**
+ * a three dimensional vector
+ * @author Tobias Domhan
+ *
+ */
+public class Vector3D {
+	/**
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
+	public Vector3D(float x, float y, float z) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	private float x=0;
+	/**
+	 * @return the x
+	 */
+	public float getX() {
+		return x;
+	}
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(float x) {
+		this.x = x;
+	}
+	/**
+	 * @return the y
+	 */
+	public float getY() {
+		return y;
+	}
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(float y) {
+		this.y = y;
+	}
+	/**
+	 * @return the z
+	 */
+	public float getZ() {
+		return z;
+	}
+	/**
+	 * @param z the z to set
+	 */
+	public void setZ(float z) {
+		this.z = z;
+	}
+	private float y=0;
+	private float z=0;
+}
