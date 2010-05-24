9d037d774ee8f9b3bc248155c5a0fc3293c596ab
tdomhan <tdomhan@258557a0-e19b-11de-9129-6395494df245>
2010-05-24 12:35:40 +0000

    autofocus handler gets fired as soon as a marker is no longer visible

---
+++ AndAR/src/edu/dhbw/andar/interfaces/MarkerVisibilityListener.java
@@ -0,0 +1,10 @@
+package edu.dhbw.andar.interfaces;
+
+/**
+ * gets notified 
+ * @author Tobi
+ *
+ */
+public interface MarkerVisibilityListener {
+	public void makerVisibilityChanged(boolean visible);
+}
